ba6944d7e25a1bf7d6d1a785b51d155827216341
Andy Chien <r960411@gmail.com>
2023-05-10 18:16:35 +0800

    pybullet using bullet client

--- packing_env/bullet_handler.py
+++ packing_env/bullet_handler.py
@@ -3,42 +3,46 @@ import time
 import numpy as np
 import pybullet as pb
 import pybullet_data
+from pybullet_utils import bullet_client as bc 
 
 class BulletHandler:
-    def __init__(self):
-        self.physics_client = pb.connect(pb.GUI)
+    def __init__(self, gui=True):
+        if gui:
+            self.pb = bc.BulletClient(connection_mode=pb.GUI)
+        else:
+            self.pb = bc.BulletClient(connection_mode=pb.DIRECT)
         self.model_path = set()
         #TODO: current_path seems useless
         self.current_path = None
         self._setup()
 
     def _setup(self):
-        pb.setGravity(0,0,-10)
-        pb.setAdditionalSearchPath(pybullet_data.getDataPath())
-        pb.loadURDF('plane.urdf')
+        self.pb.setGravity(0,0,-10)
+        self.pb.setAdditionalSearchPath(pybullet_data.getDataPath())
+        self.pb.loadURDF('plane.urdf')
         if self.current_path:
-            pb.setAdditionalSearchPath(self.current_path)
+            self.pb.setAdditionalSearchPath(self.current_path)
 
     def reset_all(self):
-        pb.resetSimulation()
+        self.pb.resetSimulation()
         self._setup()
 
     def set_model_path(self, path):
         self.model_path.add(path)
         self.current_path = path 
-        pb.setAdditionalSearchPath(path)
+        self.pb.setAdditionalSearchPath(path)
         print('[BulletHandler]: !!!!!!!!!!!!!!!!!!!path = {}'.format(path))
     
     def load_urdf(self, path, pos, quat):
         try:
             print('[BulletHandler]: path = {}'.format(path))
-            return pb.loadURDF(path, pos, quat)
+            return self.pb.loadURDF(path, pos, quat)
         except:
             for p in self.model_path:
                 try:
                     self.set_model_path(p)
                     print('[BulletHandler]: Model path swich to {}'.format(p))
-                    model_id = pb.loadURDF(path, pos, quat)
+                    model_id = self.pb.loadURDF(path, pos, quat)
                     return model_id
                 except:
                     print('[BulletHandler]: Try another path')
@@ -47,13 +51,13 @@ class BulletHandler:
     
     def load_stl(self, file, scale, pos, quat, mass=10):
         def load_stl():
-            coll_id = pb.createCollisionShape(shapeType=pb.GEOM_MESH, flags=pb.GEOM_FORCE_CONCAVE_TRIMESH, \
+            coll_id = self.pb.createCollisionShape(shapeType=pb.GEOM_MESH, flags=pb.GEOM_FORCE_CONCAVE_TRIMESH, \
                     meshScale=scale, fileName=file)
-            vis_id = pb.createVisualShape(
+            vis_id = self.pb.createVisualShape(
                 shapeType=pb.GEOM_MESH, meshScale=scale, fileName=file, 
                 rgbaColor=[0.92, 0.66, 0.33, 0.5], specularColor=[0.4, 0.4, 0.4])
         
-            model_id = pb.createMultiBody(baseMass=mass, 
+            model_id = self.pb.createMultiBody(baseMass=mass, 
                                           baseCollisionShapeIndex=coll_id, 
                                           baseVisualShapeIndex=vis_id, 
                                           basePosition=pos, 
@@ -76,22 +80,22 @@ class BulletHandler:
         
     def create_box(self, box_size, pos, quat, mass=1):
         half_extents = np.array(box_size) / 2
-        coll_id = pb.createCollisionShape(shapeType=pb.GEOM_BOX, halfExtents=half_extents)
-        vis_id = pb.createVisualShape(shapeType=pb.GEOM_BOX, halfExtents=half_extents)
-        model_id = pb.createMultiBody(mass, coll_id, vis_id, pos, quat)
+        coll_id = self.pb.createCollisionShape(shapeType=pb.GEOM_BOX, halfExtents=half_extents)
+        vis_id = self.pb.createVisualShape(shapeType=pb.GEOM_BOX, halfExtents=half_extents)
+        model_id = self.pb.createMultiBody(mass, coll_id, vis_id, pos, quat)
         return model_id
 
     def set_model_pose(self, model_id, pos, quat):
-        pb.resetBasePositionAndOrientation(model_id, pos, quat)
+        self.pb.resetBasePositionAndOrientation(model_id, pos, quat)
 
     def get_model_pose(self, model_id):
-        return pb.getBasePositionAndOrientation(model_id)
+        return self.pb.getBasePositionAndOrientation(model_id)
     
     def remove_model(self, model_id):
-        return pb.removeBody(model_id)
+        return self.pb.removeBody(model_id)
 
     def step_simulation(self, setp_cnt, realtime=False):
         for i in range (setp_cnt):
-            pb.stepSimulation()
+            self.pb.stepSimulation()
             if realtime:
                 time.sleep(1./240.)
